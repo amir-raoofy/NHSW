47a338c7123010446642aaab2f4f1ee077e43826
Amir <amir.raoofy@tum.de>
2016-12-06 03:19:28 +0100

    MOD: stencil constructors updated

--- Stencil.cpp
+++ Stencil.cpp
@@ -4,18 +4,16 @@ Stencil::Stencil(const Parameters& parameters,const FlowField& flowField,int Num
 	Stencil_( DiscreteLine(NumBuffers,0) ),
 	parameters_(parameters),
 	flowField_(flowField){}
-std::vector<float>& Stencil::SetStencil(){return Stencil_;}
-const std::vector<float>& Stencil::GetStencil() const{return Stencil_;}
 
-Stencil1D::Stencil1D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 3){}
-Stencil2D::Stencil2D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 5){}
-Stencil3D::Stencil3D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 7){}
-BoundaryStencil1D::BoundaryStencil1D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 2){}
-BoundaryStencil2D::BoundaryStencil2D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 4){}
-BoundaryStencil3D::BoundaryStencil3D(const Parameters& parameters,const FlowField& flowField):
-	Stencil(parameters, flowField, 6){}
+Stencil1D::Stencil1D(const Parameters& parameters, const FlowField& flowField, DiscreteLine& field):
+	Stencil(parameters, flowField, 3),field_(field){}
+Stencil2D::Stencil2D(const Parameters& parameters, const FlowField& flowField, DiscreteRectangle field):
+	Stencil(parameters, flowField, 5), field_(field){}
+Stencil3D::Stencil3D(const Parameters& parameters, const FlowField& flowField, DiscreteCube field):
+	Stencil(parameters, flowField, 7), field_(field){}
+BoundaryStencil1D::BoundaryStencil1D(const Parameters& parameters, const FlowField& flowField, DiscreteLine& field, DiscreteLine& boundaries):
+	Stencil(parameters, flowField, 2), field_(field), boundaries_(boundaries){}
+BoundaryStencil2D::BoundaryStencil2D(const Parameters& parameters, const FlowField& flowField, DiscreteRectangle field, DiscreteRectangle& boundaries):
+	Stencil(parameters, flowField, 4), field_(field), boundaries_(boundaries){}
+BoundaryStencil3D::BoundaryStencil3D(const Parameters& parameters, const FlowField& flowField, DiscreteCube field, DiscreteCube& boundaries):
+	Stencil(parameters, flowField, 6), field_(field), boundaries_(boundaries){}
--- Stencil.h
+++ Stencil.h
@@ -6,55 +6,68 @@ class Stencil
 {
 public:
 	Stencil(const Parameters& parameters,const FlowField& flowField, int NumBuffers);
-	//setter and getter
-	DiscreteLine& SetStencil(); 
-	const DiscreteLine& GetStencil() const; 
-
 protected:
 	DiscreteLine Stencil_;
 	const Parameters& parameters_;
 	const FlowField& flowField_;
 private:
-	
+	virtual void Operate() = 0;
+	virtual void Operate(int i) = 0;
+	virtual void Operate(int i, int j) = 0;
+	virtual void Operate(int i, int j, int k) = 0;
 };
 
 class Stencil1D: public	Stencil
 {
 public:
-	Stencil1D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteLine& field, int i) = 0;
+	Stencil1D(const Parameters& parameters, const FlowField& flowField, DiscreteLine& field);
+	virtual void Operate(int i) = 0;
+private:
+	DiscreteLine& field_;
 };
 
 class Stencil2D: public	Stencil
 {
 public:
-	Stencil2D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteRectangle& field, int i, int j) = 0;
+	Stencil2D(const Parameters& parameters, const FlowField& flowField, DiscreteRectangle field);
+	virtual void Operate(int i, int j) = 0;
+	DiscreteRectangle field_;
 };
 
 class Stencil3D: public	Stencil
 {
 public:
-	Stencil3D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteCube& field, int i, int j, int k) = 0;
+	Stencil3D(const Parameters& parameters, const FlowField& flowField, DiscreteCube field);
+	virtual void Operate(int i, int j, int k) = 0;
+private:
+	DiscreteCube field_;
 };
 
 class BoundaryStencil1D: public	Stencil{
 public:
-	BoundaryStencil1D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteLine& field, FLOAT Boundary) = 0;
+	BoundaryStencil1D(const Parameters& parameters, const FlowField& flowField, DiscreteLine& field, DiscreteLine& boundaries);
+	virtual void Operate() = 0;
+private:
+	DiscreteLine& field_;
+	DiscreteLine& boundaries_;
 };
 
 class BoundaryStencil2D: public	Stencil
 {
 public:
-	BoundaryStencil2D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteRectangle& field, DiscreteLine Boundary, int i) = 0;
+	BoundaryStencil2D(const Parameters& parameters, const FlowField& flowField, DiscreteRectangle field, DiscreteRectangle& boundaries);
+	virtual void Operate(int i) = 0;
+private:
+	DiscreteRectangle field_;
+	DiscreteRectangle& boundaries_;
 };
 
 class BoundaryStencil3D: public	Stencil
 {
 public:
-	BoundaryStencil3D(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(DiscreteCube& field, DiscreteRectangle Boundary, int i, int j) = 0;
+	BoundaryStencil3D(const Parameters& parameters, const FlowField& flowField, DiscreteCube field, DiscreteCube& boundaries);
+	virtual void Operate(int i, int j) = 0;
+private:
+	DiscreteCube field_;
+	DiscreteCube& boundaries_;
 };
