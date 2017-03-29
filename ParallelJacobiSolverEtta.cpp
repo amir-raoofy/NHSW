36fd7f26760a941c4874e0f042f563d8e6804dfb
Amir <amir.raoofy@tum.de>
2017-03-29 19:00:16 +0200

    MOD: minor change

--- ParallelJacobiSolverEtta.cpp
+++ ParallelJacobiSolverEtta.cpp
@@ -4,10 +4,10 @@
 ParallelJacobiSolverEtta::ParallelJacobiSolverEtta(const Parameters& parameters, FlowField& flowField, CommunicationManager &communicationManager): JacobiSolverEtta(parameters, flowField), communicationManager_(communicationManager){}
 
 void ParallelJacobiSolverEtta::iterate(){
-	communicationManager_.communicteEtta();
-	MPI_Barrier(parameters_.topology.communicator);
 	updateDomain();
 	updateBoundary();
+	communicationManager_.communicteEtta();
+	MPI_Barrier(parameters_.topology.communicator);
 	//updateError();
 	//etta_old_.swap(flowField_.SetEtta());
 }
