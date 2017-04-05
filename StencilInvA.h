f907d6af5f4f3cfc63c44c99b04b0fbd7b79b5a0
Amir <amir.raoofy@tum.de>
2017-04-05 22:27:07 +0200

    REM: old stuff removed

--- StencilInvA.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Stencil.h"
-
-class StencilInvA: public Stencil
-{
-public:
-	StencilInvA(const Parameters& parameters,const FlowField& flowField);
-	virtual void Operate(int i=0, int j=0, int k=0);
-};
