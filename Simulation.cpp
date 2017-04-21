b53ee6bc04a89c4406905771f7e6380d9e0bdefa
Amir <amir.raoofy@tum.de>
2017-04-21 15:36:57 +0200

    MOD: time measurement improved

--- Simulation.cpp
+++ Simulation.cpp
@@ -29,20 +29,22 @@ void Simulation::printTimeMeasurements(){
 		std::cout << std::left << std::setw(52) << "|Time spent in all iterations: " 					<< std::setw(11)	<< T0_ 						<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Average time spent in one TimeStep: " 				<< std::setw(11)	<< T0_/it0_ 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Time spent in 1D solvers in all TimeSteps: " 		<< std::setw(11)	<< T1_ 						<< " |" << std::endl;
-		std::cout << std::left << std::setw(52) << "|Average time spent in 1D solvers in one TimeStep: "<< std::setw(11)	<< (T1_)/it0_ 				<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Average time spent in 1D solvers in one TimeStep: "<< std::setw(11)	<< T1_/it0_ 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|portion of 1D solvers in all iterations (%): " 	<< std::setw(11)	<< 100*T1_/T0_ 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Total iteration count in all 1d solvers: "			<< std::setw(11)	<< it1_ 					<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Average iteration count for 1d solves: "			<< std::setw(11)	<< it1_/(FLOAT)(it0_*N) 	<< " |" << std::endl;
-		std::cout << std::left << std::setw(52) << "|Average time for one iteration in 1d solves: "		<< std::setw(11)	<< T1_/ it1_ 				<< " |" << std::endl;
-		std::cout << std::left << std::setw(52) << "|Average time for one 1d solve: "					<< std::setw(11)	<< T1_/ (it0_*N) 			<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Accumulative iteration count for 1d solves: "		<< std::setw(11)	<< it1_/(FLOAT)N	 		<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Iterations for all 1d solves in one time step: "	<< std::setw(11)	<< it1_/(FLOAT)it0_	 		<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Average time for one iteration in 1d solves: "		<< std::setw(11)	<< T1_/ it1_				<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Average time for one 1d solve: "					<< std::setw(11)	<< T1_/ (FLOAT)(it0_*N) 	<< " |" << std::endl;
+		std::cout << std::left << std::setw(52) << "|Accumulative average time for one 1d solve: "		<< std::setw(11)	<< T1_/ (N) 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Time spent in 2D solver in all TimeSteps: " 		<< std::setw(11)	<< T2_ 						<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Average time spent in 2D solver in one TimeStep: "	<< std::setw(11)	<< T2_/it0_ 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|portion of 2D solvers in all iterations (%): " 	<< std::setw(11)	<< 100*T2_/T0_ 				<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Total iteration count for all 2d solves: "			<< std::setw(11)	<< it2_ 					<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Average iteration count for 2d solves: "			<< std::setw(11)	<< it2_/(FLOAT)it0_			<< " |" << std::endl;
 		std::cout << std::left << std::setw(52) << "|Average time for one iteration in 2d solves: "		<< std::setw(11)	<< T2_/it2_ 				<< " |" << std::endl;
-		std::cout << std::left << std::setw(52) << "|Average time for one 2d solve: "					<< std::setw(11)	<< T2_/it0_ 				<< " |" << std::endl;
-		std::cout << "\033[1;31m =============================================================== \033[0m"   	<< std::endl;
+		std::cout << "\033[1;31m =============================================================== \033[0m"   													<< std::endl;
 
 }
 
